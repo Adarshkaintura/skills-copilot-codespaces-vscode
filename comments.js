19a20df21339526693b1c3314e00596f47f38411
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-02-25 16:19:53 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,13 @@
 // Create web server
+function skillsMember(){
+    return{
+        restric:'E',
+        templateURL:'members/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs:'vm',
+        bindToController:true,
+        scope:{
+            member:'='
+        }
+    };
+}
